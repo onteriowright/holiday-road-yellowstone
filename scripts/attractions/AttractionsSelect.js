45a3a030c702127b1fa7ac8062d59b5815ab5f7a
philipDmartin <philipnathan788@gmail.com>
2019-12-17 09:20:13 -0600

    attraction.js

--- scripts/attractions/AttractionsSelect.js
+++ scripts/attractions/AttractionsSelect.js
@@ -1,10 +1,46 @@
+import useAttractions from "./attractions/AttractionsSelect.js"
+
 const contentTarget = document.querySelector('.attractionsDropdown')
+const eventHub = document.querySelector(".container")
 
 const AttractionsSelect = () => {
-  const render = attractionsCollection => {
+    const attractions = useAttractions()
+
+// eventHub.addEventListener("change", changeEvent => {
+//         if (changeEvent.target.id === "attractionsSelect") {
+//             const selectedAttractions = changeEvent.target.value
+//             const message = new CustomEvent("attractionsSelected", {
+//                 detail: {
+//                     attractions: selectedAttractions
+//                 }
+//             })
+         
+//             eventHub.dispatchEvent(message)
+//         }
+//     })   
+
+// eventHub.addEventListener("change", changeEvent => {
+//         if (changeEvent.target.classList.contains("dropDown")) {
+//             const selectedAttractions = changeEvent.target.value
+
+//             const message = new CustomEvent("attractionsSelected", {
+//                 detail: {
+//                     attractions: selectedAttractions
+//                 }
+//             })
+
+//             eventHub.dispatchEvent(message)
+//         }
+//     })
+
+    const render = attractionsCollection => {
     contentTarget.innerHTML = `
             <select class="dropDown" id="attractionsSelect">
-    <option>attractions</option>
+    ${
+                    attractions.map(attractions =>
+                        `<option value="${attractions}">Attractions ${attractions}</option>`
+                    )
+                }
             </select>
         `
   }
