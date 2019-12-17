9544fb9aa66fe3911d75323e538fd1656af13a24
DanielBradleyBennett <danielbradleybennett@gmail.com>
2019-12-17 11:20:35 -0600

    preview section

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 import AttractionsSelect from "./attractions/AttractionsSelect.js"
 import { getAttractions } from "./attractions/AttractionProvider.js"
+import { AttractionsListComponent } from "./attractions/AttractionsList.js"
 
    
 
-getAttractions().then(AttractionsSelect)
+getAttractions().then(AttractionsSelect).then(AttractionsListComponent)
 
