52730c21099534355e222c478d8d453acee634ab
philipDmartin <philipnathan788@gmail.com>
2019-12-18 15:51:37 -0600

    new master

--- scripts/attractions/asideAttractionsComponent.js
+++
@@ -1,9 +0,0 @@
-export const AsideAttractionsComponent = data => {
-  return `
-    <section>
-      <div>${data.businessName}</div>
-      <div>${data.city}</div>
-      <div>${data.state}</div>
-    </section>
-  `;
-};
