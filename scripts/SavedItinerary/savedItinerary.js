eb03665e73973214c31cd5430fd29d7710b22a62
philipDmartin <philipnathan788@gmail.com>
2019-12-20 14:32:45 -0600

    done

--- scripts/SavedItinerary/savedItinerary.js
+++ scripts/SavedItinerary/savedItinerary.js
@@ -2,9 +2,9 @@
 const itineraryAside = it => {
   return `
   <section class="itineraryCard">
-  <div class="parkName itinerary-info"><h3 class="itColor">Park: </h3><h4>${it.parkName}</h4></div>
-  <div class="attractionName itinerary-info"><h3 class="itColor">Attraction: </h3><h4>${it.attractionName}</h4></div>
-  <div class="eateryName itinerary-info"><h3 class="itColor">Eatery: </h3><h4>${it.eateryName}</h4></div>
+  <div class="parkName itinerary-info"><h3>Park: </h3><h4>${it.parkName}</h4></div>
+  <div class="attractionName itinerary-info"><h3>Attraction: </h3><h4>${it.attractionName}</h4></div>
+  <div class="eateryName itinerary-info"><h3>Eatery: </h3><h4>${it.eateryName}</h4></div>
   `;
 };
 export default itineraryAside;
